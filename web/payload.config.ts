02d850ce875367945af4f090d530d5b8823ced19
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-07-17 02:54:22 +0700

    build(paylaod): add vercel object storage

--- web/payload.config.ts
+++ web/payload.config.ts
@@ -2,14 +2,15 @@
 import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { fileURLToPath } from "url";
+import { IdeasPage } from "./collections/IdeasPage";
+import { Media } from "./collections/Media";
 import { Pages } from "./collections/Pages";
 import { Users } from "./collections/Users";
-import { Media } from "./collections/Media";
-import { IdeasPage } from "./collections/IdeasPage";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -58,6 +59,11 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 });
