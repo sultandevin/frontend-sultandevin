da10e1ddac46c2bb500197772a823a871636c321
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-07-17 03:48:03 +0700

    fix: add favicon

--- web/app/(app)/ideas/(posts-wrapper)/page.tsx
+++ web/app/(app)/ideas/(posts-wrapper)/page.tsx
@@ -1,5 +1,10 @@
 import IdeasPosts from "@/lib/modules/ideas/ideas-posts";
 
+export const metadata = {
+  title: "Ideas | Suitmedia",
+  description: "Explore various ideas and posts",
+};
+
 const IdeasPage = async (props: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) => {
---
+++ web/lib/modules/home/hero.tsx
@@ -0,0 +1,38 @@
+import { buttonVariants } from "@/components/ui/button";
+import Container from "@/components/ui/container";
+import { cn } from "@/utils/cn";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <main className="relative">
+      <Container className="flex min-h-[90vh] justify-end">
+        <div className="flex max-w-lg flex-col gap-3 text-white">
+          <h1 className="text-4xl font-bold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam earum
+          </p>
+          <Link
+            href="/ideas"
+            className={cn(buttonVariants({ variant: "default" }), "w-fit")}
+          >
+            Explore Ideas
+          </Link>
+        </div>
+      </Container>
+      <Image
+        src="/hero.webp"
+        alt="Hero Image"
+        fill
+        sizes="80%"
+        className="pointer-events-none -z-20 object-cover select-none"
+      />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-black to-transparent" />
+    </main>
+  );
+};
+
+export default Hero;
